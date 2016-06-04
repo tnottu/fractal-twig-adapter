47e682dfc51ac1a8acff645e587e2cd2ab6d4bf9
Tomi <tnottu@gmail.com>
2016-06-04 20:46:27 +0300

    Add custom loader

--- adapter.js
+++ adapter.js
@@ -1,16 +1,58 @@
 'use strict';
 
-const twig = require('twig');
+const Path = require('path');
+const Twig = require('twig');
 
 module.exports = function(source, config){
 
-    let instance = twig;
+	let instance = Twig.twig;
+	let viewCache = null;
 
-    return {
-        engine: instance,
-        render: function(path, str, context, meta){
-            let template = instance({ data: str });
-            return Promise.resolve(template.render(context));
-        }
-    }
+	Twig.cache = false;
+
+	/**
+	 * Register a custom loader for `@component` reference handles
+	 * More information: https://github.com/twigjs/twig.js/pull/301
+	 */
+	Twig.extend(function(Twig) {
+		Twig.Templates.registerLoader('fractal', function(name, params) {
+			params.data = viewCache[params.id];
+			return new Twig.Template(params);
+		});
+	});
+
+	/**
+	 * Load and cache view contents
+	 */
+	function loadViews() {
+		viewCache = {};
+		for (let view of source.flattenDeep()) {
+			viewCache[`@${view.alias}`] = view.content;
+		}
+	}
+
+	/**
+	 * Refresh cache on changes
+	 */
+	source.on('loaded', loadViews);
+	source.on('changed', loadViews);
+
+	return {
+		engine: instance,
+		render: function(path, str, context, meta) {
+
+			if (!viewCache) { loadViews(); }
+
+			let template = instance({
+				method: 'fractal',
+				name: `@${Path.parse(path).name}`,
+				allowInlineIncludes: true,
+				async: false
+			});
+
+			return Promise.resolve(template.render(context));
+
+		}
+	};
+
 };
