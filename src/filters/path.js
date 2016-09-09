c99378a549677bda7b0b34bf818a6cdd2d167a94
Tomi <tnottu@gmail.com>
2016-09-09 13:07:41 +0300

    Fix path helper

--- src/filters/path.js
+++ src/filters/path.js
@@ -7,7 +7,16 @@ module.exports = function(fractal) {
 	return function(path) {
 		const env = this.context._env;
 		const request = env && env.request;
+		const mountPath = fractal.web.get('static.mount');
+		let _path;
 
-		return (!env || env.server) ? path : utils.relUrlPath(path, _.get(request, 'path', '/'), fractal.web.get('builder.urls'));
+		if (!env || env.server) {
+			_path = path;
+			_path = (mountPath) ? `/${mountPath}${_path}` : _path;
+		} else {
+			_path = utils.relUrlPath(path, _.get(request, 'path', '/'), fractal.web.get('builder.urls'));
+		}
+
+		return _path;
 	};
 };
